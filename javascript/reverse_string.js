dab0fb9c229d2eef58210eacba2f3f7783a571de
Daniel Yim <daniel.yim96@gmail.com>
2023-02-23 10:47:12 -0500

    commit tree d72fff178f501b85e9f410cbe649fa346ec81c68

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,9 @@
 function reverseString(str) {
   // type your code here
+  let string = str.
+  for (let i = str.length; i < 0, i--)
+        return 
+  }
 }
 
 if (require.main === module) {
@@ -17,3 +21,10 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+
+// change string into an array
+// split the array into individual letters
+// then loop from the last letter to the first letter
+// return each letter 
+// change array back into an string
